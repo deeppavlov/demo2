eda0e371d225f3370b9286b91fa4b6265f7d35cb
blakhard <vlad.abdulmianov@gmail.com>
2019-05-31 16:51:58 +0300

    update: new rules for eslint, README.md

--- src/components/BotChat/index.tsx
+++ src/components/BotChat/index.tsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
-export default class BotChat extends Component {
+interface OwnProps {
+    skillName: string;
+    skillDesc: string;
+}
+
+type Props = OwnProps;
+
+export default class BotChat extends Component<Props> {
 
     render() {
+        const { skillName, skillDesc } = this.props;
         return (
-            <div>
-                
-            </div>
+            <section>
+                <div>
+                    <h1>{skillName}</h1>
+                    <p>{skillDesc}</p>
+                </div>
+            </section>
         )
     }
 }
