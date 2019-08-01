83b111b8c95eb136c66b0390c65b76f9ae2e0fa3
blakhard <vlad.abdulmianov@gmail.com>
2019-08-01 16:12:26 +0300

    Updates

---
+++ precache-manifest.309071904a723f518702aebd4145f103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb91f395fdf74cbf160f417136ac92f",
+    "url": "/deepmipt/demo2/index.html"
+  },
+  {
+    "revision": "166bfeb269daf79f86ce",
+    "url": "/deepmipt/demo2/static/css/main.47acca2f.chunk.css"
+  },
+  {
+    "revision": "d6bc594c5c143bdc4b80",
+    "url": "/deepmipt/demo2/static/js/2.8e7e29cf.chunk.js"
+  },
+  {
+    "revision": "166bfeb269daf79f86ce",
+    "url": "/deepmipt/demo2/static/js/main.66700a02.chunk.js"
+  },
+  {
+    "revision": "72678fa7bba9bcf30932",
+    "url": "/deepmipt/demo2/static/js/runtime~main.27c2daf9.js"
+  },
+  {
+    "revision": "13c4e31c1ed8ad953b9cb529c0d5a1ea",
+    "url": "/deepmipt/demo2/static/media/DeepPavlov.13c4e31c.png"
+  },
+  {
+    "revision": "96c786df0a381cae7847ac6b2c94c265",
+    "url": "/deepmipt/demo2/static/media/header.96c786df.png"
+  }
+]);
