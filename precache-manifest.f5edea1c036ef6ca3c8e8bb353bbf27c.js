ad11f1b1f9af53f9f9dc1d1ecc827026d0f268fc
Fedor Ignatov <ignatov.fedor@gmail.com>
2020-09-03 17:59:21 +0300

    Updates

--- precache-manifest.f5edea1c036ef6ca3c8e8bb353bbf27c.js
+++ precache-manifest.f5edea1c036ef6ca3c8e8bb353bbf27c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1fc8c5e7cbd7788ce40b8ef65f098402",
+    "revision": "d05379f05036333648f943b84fedb774",
     "url": "/index.html"
   },
   {
-    "revision": "fd4c20a6b75f235e144c",
-    "url": "/static/css/main.be971e30.chunk.css"
+    "revision": "650e6430f47d331cd2dc",
+    "url": "/static/css/main.f129185b.chunk.css"
   },
   {
-    "revision": "2324b06bb35854fa49b0",
-    "url": "/static/js/2.c4104a83.chunk.js"
+    "revision": "74a3b64871002263b3b0",
+    "url": "/static/js/2.5452ea74.chunk.js"
   },
   {
     "revision": "8d5f7589a9027e05629570c8dd3da9dd",
-    "url": "/static/js/2.c4104a83.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.5452ea74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd4c20a6b75f235e144c",
-    "url": "/static/js/main.fcff8841.chunk.js"
+    "revision": "650e6430f47d331cd2dc",
+    "url": "/static/js/main.7d4757f9.chunk.js"
   },
   {
     "revision": "1536687187469b0b542c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e751d86828784d20eb8bb0bda718ed03.js"
+  "/precache-manifest.f5edea1c036ef6ca3c8e8bb353bbf27c.js"
 );
 
 self.addEventListener('message', (event) => {
