8069131c94e06340f440dc8df0bb50767398c132
Nikolay <Kolpakov.na@phystech.edu>
2023-07-24 14:24:41 +0300

    update integration examples

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -14,7 +14,7 @@ export const scripts = {
       [Snippets.cli]:
         "python -m deeppavlov interact emotions_xlm_roberta_base -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(emotions_xlm_roberta_base, download=True, install=True)
+model = build_model('emotions_xlm_roberta_base', download=True, install=True)
 model(['This makes me smile'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi emotions_xlm_roberta_base -di",
@@ -23,7 +23,7 @@ model(['This makes me smile'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact sentiments_xlm_roberta_base -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(sentiments_xlm_roberta_base, download=True, install=True)
+model = build_model('sentiments_xlm_roberta_base', download=True, install=True)
 model(['This month was a bad month for Kobold Quarterly'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi sentiments_xlm_roberta_base -di",
@@ -37,7 +37,7 @@ model(['This month was a bad month for Kobold Quarterly'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact topics_xlm_roberta_base -di",
       [Snippets.python]: `from deeppavlov import build_model 
-model = build_model(topics_xlm_roberta_base, download=True, install=True) 
+model = build_model('topics_xlm_roberta_base', download=True, install=True) 
 model(['Great shirt, the talls are perfect length'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi topics_xlm_roberta_base -di",
@@ -46,7 +46,7 @@ model(['Great shirt, the talls are perfect length'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact insults_xlm_roberta_base -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(insults_xlm_roberta_base, download=True, install=True)
+model = build_model('insults_xlm_roberta_base', download=True, install=True)
 model(['Was in the city yesterday - didn’t notice anything strange'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi insults_xlm_roberta_base -di",
@@ -55,7 +55,7 @@ model(['Was in the city yesterday - didn’t notice anything strange'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact intents_distilbert_base_multi -di",
       [Snippets.python]: `from deeppavlov import build_model 
-model = build_model(intents_distilbert_base_multi, download=True, install=True) 
+model = build_model('intents_distilbert_base_multi', download=True, install=True) 
 model(['Turn on some dance music to lift our mood'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi intents_distilbert_base_multi -di",
@@ -66,19 +66,19 @@ model(['Turn on some dance music to lift our mood'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact entity_extraction_en -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(entity_extraction_en, download=True, install=True)
+model = build_model('entity_extraction_en', download=True, install=True)
 model(['Axiom Space announced SpaceX will fly additional private crew missions to and from the Station through 2023.'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi entity_extraction_en -di",
     },
     namedEntityRecognition: {
       [Snippets.cli]:
-        "python -m deeppavlov interact ner_xlm_roberta_base -di",
+        "python -m deeppavlov interact ner_bert_base_mult -di",
       [Snippets.python]: `from deeppavlov import build_model     
-model = build_model(ner_distilbert-base-multi, download=True, install=True)
-model(['Let’s order pizza delivery at Marty and Ricky’s for 3 pm.'])`,
+model = build_model('ner_bert_base_mult', download=True, install=True)
+model(['John, did you set a weekly appointment reminder? Can’t find it in my inbox.'])`,
       [Snippets.restApi]:
-        "python -m deeppavlov riseapi ner_xlm_roberta_base -di",
+        "python -m deeppavlov riseapi ner_bert_base_mult -di",
     },
     partOfSpeech: {
       [Snippets.cli]: "",
@@ -96,15 +96,19 @@ model(['Let’s order pizza delivery at Marty and Ricky’s for 3 pm.'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact kbqa_lcquad2 -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(kbqa_lcquad2, download=True, install=True)
+model = build_model('kbqa_lcquad2', download=True, install=True)
 model(['When did Jean-Paul Sartre move to Le Havre?'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi kbqa_lcquad2 -di",
     },
     readingComprehension: {
-      [Snippets.cli]: "We will add the information here later.",
-      [Snippets.python]: "We will add the information here later.",
-      [Snippets.restApi]: "We will add the information here later. ",
+      [Snippets.cli]:
+        "python -m deeppavlov interact qa_squad2_bert -di",
+      [Snippets.python]: `from deeppavlov import build_model
+model = build_model('qa_squad2_bert', download=True, install=True)
+model(['DeepPavlov is a library for NLP and dialog systems.'], ['What is DeepPavlov?'])`,
+      [Snippets.restApi]:
+        "python -m deeppavlov riseapi qa_squad2_bert -di",
     },
   },
   openDomainQA: {
@@ -112,7 +116,7 @@ model(['When did Jean-Paul Sartre move to Le Havre?'])`,
       [Snippets.cli]:
         "python -m deeppavlov interact en_odqa_infer_wiki -di",
       [Snippets.python]: `from deeppavlov import build_model
-model = build_model(en_odqa_infer_wiki, download=True, install=True)
+model = build_model('en_odqa_infer_wiki', download=True, install=True)
 model(['When did the first moon landing happen?'])`,
       [Snippets.restApi]:
         "python -m deeppavlov riseapi en_odqa_infer_wiki -di",
