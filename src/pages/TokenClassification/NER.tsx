704e2d8f6b947a1a791ef23b2cc4e9a0db18d56d
LogicZMaksimka <savkin-max@mail.ru>
2024-07-09 14:11:40 +0300

    upd NER labels

--- src/pages/TokenClassification/NER.tsx
+++ src/pages/TokenClassification/NER.tsx
@@ -33,31 +33,47 @@ const config: BaseSkillProps<StoreReq, Res> = {
   examples: [
     {
       question:
-        "Computer Sciences Corp., El Segundo, Calif., the capitalization of which exceeds 150 million dollars, is no longer able to access its page on Facebook and Instagram developed by Meta. Meta signalled this move would be coming as a response to this company's lobbying of the Online News Act, Bill C-18. According to the annual report, after the adoption of these laws, the number of Meta users decreased by 10%.",
+        "Музей-квартира Владимира Ивановича Немировича-Данченко была создана как филиал Музея МХАТ постановлением Совнаркома СССР от 25 апреля 1943 года в целях увековечения памяти одного из основателей и руководителей Московского Художественного театра. В доме 5 в Глинищевском переулке Немирович-Данченко прожил последние пять лет своей жизни."
     },
     {
       question:
-        "On this warm and sunny and a lovely day for a walk in the Central park, David was in a hurry for an interview at the Groggle company. He woke up early and didn't have time to have a proper breakfast, only ate two cookies. David lives in his brother's house with a bathroom, a bedroom and a living room, outside the city. But the problem is that the Uber taxi does not go there.",
+        "Tesla, as of 2023, is the world's most valuable automaker. In 2022, the company led the battery electric vehicle market, with 18% share. Its subsidiary Tesla Energy develops and is a major installer of photovoltaic systems in the United States. Tesla Energy is one of the largest global suppliers of battery energy storage systems with 6 gigawatt-hours installed in 2022."
     },
     {
       question:
-        "À la fin 2019, le Louvre conserve plus de 500000 œuvres, dont plus de 36000 étaient exposées. Elles présentent l'art occidental du Moyen Âge à 1848, celui des civilisations antiques qui l'ont précédé et influencé (orientales, égyptienne, grecque, étrusque et romaine), les arts des premiers chrétiens et de l'islam.",
+        "Члены Американской академии киноискусств решили присудить режиссеру Дэвиду Линчу почетную премию Оскар за выдающийся вклад в кинематограф, сообщается на сайте академии. Церемония награждения пройдет 27 октября в развлекательном комплексе Hollywood and Highland Center в Лос-Анджелесе (штат Калифорния, США)."
     },
     {
       question:
-        "Cuenta con un PIB nominal de 142 223 millones € y un PIB per cápita nominal de 30 619 €, lo que representa un PIB PPA per cápita de 36 240 €, 11​ siendo la segunda área metropolitana francesa en actividad económica y la decimoséptima mundial después de Londres, París, Rin-Ruhr, Ámsterdam.",
+        "The National Museum of African American Music (NMAAM) located at 510 Broadway, Nashville is the only museum of its kind dedicated to preserving and celebrating the history of Black music in America."
     },
     {
       question:
-        "Tesla, as of 2023, is the world's most valuable automaker. In 2022, the company led the battery electric vehicle market, with 18% share. Its subsidiary Tesla Energy develops and is a major installer of photovoltaic systems in the United States. Tesla Energy is one of the largest global suppliers of battery energy storage systems with 6 gigawatt-hours installed in 2022.",
+        "I am excited to invite you to my birthday party on May 16th, taking place at delightful Tasty Food restaurant located at 50431 Sherry Lodge, Mitchellberg"
     },
     {
       question:
-        'Члены Американской академии киноискусств решили присудить режиссеру Дэвиду Линчу почетную премию "Оскар" за выдающийся вклад в кинематограф, сообщается на сайте академии. Церемония награждения пройдет 27 октября в развлекательном комплексе Hollywood and Highland Center в Лос-Анджелесе (штат Калифорния, США).',
+        "Last year, US corporations occupied 80% of Tianjin City's foreign merchant investment, with operating volume occupying 32%, but profits occupying 45%"
     },
     {
       question:
-        "Grandpa gave Monica the shopping list. Ne peut pas trouver la lettre de myfriend@somemail.country. Me gustaría comerme una hamburguesa de res doble con papas fritas. Geben sie mir ihre adresse und telefonnummer. Включи поп музыку, так будет веселее играть в Tetris",
+        "Cuenta con un PIB nominal de 142223 millones € y un PIB per cápita nominal de 30619 €, lo que representa un PIB PPA per cápita de 36240 €, 11​ siendo la segunda área metropolitana francesa en actividad económica y la decimoséptima mundial después de Londres, París, Rin-Ruhr, Ámsterdam"
+    },
+    {
+      question:
+        "Soichiro Honda's picture now hangs with Henry Ford's in the US Automotive Hall of Fame, and the game-show “Jeopardy'' is soon to be Sony-owned"
+    },
+    {
+      question:
+        "Свидетельство о регистрации по месту пребывания выдано Иванову Петру Сергеевичу 20 апреля 1985 года о том, что он зарегистрирован по месту пребывания по адресу: Московская обл., г. Красногорск, Красногорский пер., д. 35, кв. 9."
+    },
+    {
+      question:
+        "Two other firms, Ovonic Imaging Systems Inc. of Southfield, Mich., and Magnascreen Corp. of Pittsburgh are developing a variation of the flat-panel screens called active-matrix liquid crystal displays"
+    },
+    {
+      question:
+        "Chen points out that every year more than 900,000 residents of Taipei - or more than 30 % of its population - travel overseas"
     }
   ],
   api: api("https://7039.deeppavlov.ai/model"),
